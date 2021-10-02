3ed7f4c7da921c1561bfa78615864457252fab58
GEESE <giesdb@gmail.com>
2021-10-02 16:00:10 +0200

    removed unnecessary resolutions from package.json

--- app/types/types.d.ts
+++ app/types/types.d.ts
@@ -2,7 +2,6 @@ declare module 'react-native-vector-icons/FontAwesome'
 declare module 'react-native-vector-icons/Feather'
 declare module '*.png'
 declare module '*.json'
-declare module '*.ttf'
 declare module '@env' {
     export const API_URL: string
     export const APPLICATION_ID: string
