b3cd00db434754cf43c65c0de4a1859ceb466b8a
GEESE <giesdb@gmail.com>
2021-10-19 14:23:47 +0200

    Started working on some more generic types in api-types

--- packages/api-types/types/base.ts
+++ packages/api-types/types/base.ts
@@ -1,3 +1,39 @@
+import { User } from './user'
+
 export type Class<T> = new (...args: any[]) => T
 
 export type Instance = { [key: string]: any }
+
+export type Primitive = number | string | boolean
+
+export type IfEquals<X, Y, A = X, B = never> = (<T>() => T extends X
+    ? 1
+    : 2) extends <T>() => T extends Y ? 1 : 2
+    ? A
+    : B
+
+export type ReadonlyKeys<T> = {
+    [P in keyof T]-?: IfEquals<
+        { [Q in P]: T[P] },
+        { -readonly [Q in P]: T[P] },
+        never,
+        P
+    >
+}[keyof T]
+
+export type WritableKeys<T> = {
+    [P in keyof T]: IfEquals<
+        { [Q in P]: T[P] },
+        { -readonly [Q in P]: T[P] },
+        P,
+        never
+    >
+}[keyof T]
+
+export type ObjectKeys<T> = {
+    [P in WritableKeys<T>]: IfEquals<T[P], Primitive, T[P]>
+}
+
+type a = User
+type x = ObjectKeys<User>
+type y = WritableKeys<User>
