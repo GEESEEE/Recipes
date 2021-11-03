c8e939dd24e442456e893258b4f012343dfe2ca0
GEESE <giesdb@gmail.com>
2021-11-03 19:46:01 +0100

    Moved listitem to molecules

--- packages/app/src/components/atoms/index.ts
+++ packages/app/src/components/atoms/index.ts
@@ -3,7 +3,6 @@ import Button from './Button'
 import IconButton from './IconButton'
 import Error from './Error'
 import DropdownMenu, { DropdownItem } from './DropdownMenu'
-import ListItem from './ListItem'
 import Editable from './Editable'
 import TextInputWithTitle from './TextInputWithTitle'
 import PressableTextWithElement from './PressableTextWithElement'
@@ -17,7 +16,6 @@ export {
     DropdownMenu,
     DropdownItem,
     Loading4Dots,
-    ListItem,
     Editable,
     TextInputWithTitle,
     PressableTextWithElement,
