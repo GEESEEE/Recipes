2b68a981f58e884a8b9cf95401fcadea65aac27a
GEESE <giesdb@gmail.com>
2021-11-14 13:58:12 +0100

    Fixed interval of orphan ingredients job

--- packages/api/src/config/bree.ts
+++ packages/api/src/config/bree.ts
@@ -1,14 +1,12 @@
 import path from 'path'
 import Bree from 'bree'
 
-console.log('Bree Config')
-
 const bree = new Bree({
     root: path.join(__dirname, '../jobs'),
     jobs: [
         {
             name: 'orphan-ingredients',
-            interval: '10s',
+            interval: '24h',
         },
     ],
 })
